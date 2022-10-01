5ea0a11b597b0e27ae7997ecfb4759a85c482f3c
taelliee <elitza_@mail.bg>
2022-10-01 21:14:28 +0300

    mini games

---
+++ Guess A Number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Guess_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Guess a number between 1 and 100: ");
+                string input = Console.ReadLine();
+                bool isValid = int.TryParse(input, out int guessedNumber);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else if (guessedNumber > computerNumber)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else if (guessedNumber < computerNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Rock Paper Scissors/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Rock = "Rock";
+            const string Paper = "Paper";
+            const string Scissors = "Scissors";
+
+            Console.Write("Choose rock[r], paper[p] or scissors[s]...");
+            string input = Console.ReadLine();
+
+            Random random = new Random();
+            int computerRandomNumber = random.Next(1, 4);
+            string computersChoice = null;
+            string playersChoice;
+
+            switch (computerRandomNumber)
+            {
+                case 1:
+                    computersChoice = Rock; break;
+                case 2:
+                    computersChoice = Paper; break;
+                case 3:
+                    computersChoice = Scissors; break;
+            }
+
+            if (input.ToLower() == "rock" || input.ToLower() == "r")
+            {
+                playersChoice = Rock;
+            }
+            else if (input.ToLower() == "paper" || input.ToLower() == "p")
+            {
+                playersChoice = Paper;
+            }
+            else if (input.ToLower() == "scissors" || input.ToLower() == "s")
+            {
+                playersChoice = Scissors;
+            }
+            else
+            {
+                Console.WriteLine("Ivalid input. Try again...");
+                return;
+            }
+
+            Console.WriteLine($"The computer chose {computersChoice}.");
+
+            if (playersChoice == computersChoice)
+            {
+                Console.WriteLine("The game was a draw.");
+            }
+            else
+            {
+                if (playersChoice == Rock && computersChoice == Scissors || 
+                    playersChoice == Paper && computersChoice == Rock ||
+                    playersChoice == Scissors && computersChoice == Paper)
+                {
+                    Console.WriteLine("You won!");
+                }
+                else
+                {
+                    Console.WriteLine("You lost.");
+                }
+            }
+
+        }
+    }
+}
